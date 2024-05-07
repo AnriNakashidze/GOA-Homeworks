629bf183347d5b43492a767d798d025e443dc0f3
AnriNakashidze <raylassagna505@gmail.com>
2024-05-07 20:27:05 +0400

    first commit

---
+++ Day 1/Lesson 1.py
@@ -0,0 +1,14 @@
+name ="Anri" #str (string)
+surname ="Nakashidze" 
+age = 16 #int (integer) (მთელი რიცხვი)
+height = 190.2 #float (ათწილადი)
+knows_programming = True
+is_ugly = False #snakecase
+#isUgly = False #cameLcase
+
+
+print(name + " " + str(age) + " " + str(height) + " " + "Knows Programming," + " " + str(knows_programming) + " " + "Is Ugly," + " " + str(is_ugly))
+
+#string = ბრჭყალებში მოცემული სიმბოლოები
+
+
