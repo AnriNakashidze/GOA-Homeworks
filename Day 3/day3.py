7c0ba7acb595f9abbca6fefa47db3b2189d8e5df
AnriNakashidze <raylassagna505@gmail.com>
2024-05-17 22:45:23 +0400

    second commit

---
+++ Day 3/day3.py
@@ -0,0 +1,25 @@
+# #შეკრება
+# print(125 + 984)
+
+# #გამოკლება
+# print(125 - 984)
+
+# #გამრავლება
+# print(125 * 984)
+
+# #გაყოფა
+# print(125 / 984)
+
+
+#ტერმინალი არის სადაც იწერება ბრძანებები პითონის კოდების გასაშვებად.
+#int (integer) (მთელი რიცხვი)
+#float (ათწილადი)
+#string = ბრჭყალებში მოცემული სიმბოლოები
+
+
+
+
+Mentor = "Luka"
+Is_a_good_mentor = True
+
+print("Mentor:" + Mentor + ", " + "Is a good mentor:" + str(Is_a_good_mentor))
