d8a7b66b153865b57f66f3a2290729e1165b042d
AnriNakashidze <raylassagna505@gmail.com>
2024-05-19 21:43:05 +0400

    second commit

---
+++ Day 4/day4.py
@@ -0,0 +1,33 @@
+#პროდუქტი/ნივთი და რაოდენობა
+price = 99.99
+quantity = 20
+
+#საბოლოო ფასი
+total_price = price * quantity
+print("THE PRICE IN TOTAL:", total_price)
+
+
+#ასაკები
+Son = 17
+Father = 45
+
+#ასაკები ათ წელიწადში
+print( "AGES OF THE FATHER AND THE SON IN 10 YEARS", "Son:", Son + 10, "Father:", Father + 10)
+
+#ცვლადები
+num1 = 120
+num2 = 9191
+
+#შეკრება, გამოკლება, გამრავლება, გაყოფა.
+print(num1 + num2, num1 - num2, num1 * num2, num1 / num2)
+
+#სახელი და ასაკი
+Name = "Anri"
+Age = 17
+print("Saxeli:", Name, "Asaki:", Age)
+
+#ჩემი და მამაჩემის ასაკები
+my_age = 17
+dads_age = 45
+difference = dads_age - my_age
+print("Chemi Asaki 10 Weliwadshi:", my_age + 10, "Mamachemis Asaki 10 Weliwadshi:", dads_age + 10, "Asakebs Shoris Sxvaoba:", difference)
