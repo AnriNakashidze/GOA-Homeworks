821ed84c1e8b1ed28c9e7127a536d490c4be6ae7
AnriNakashidze <raylassagna505@gmail.com>
2024-06-03 01:23:10 +0400

    second commit

---
+++ Day 10/day10.py
@@ -0,0 +1,29 @@
+# for i in range(0, 21):
+#     print(i)
+
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# num1 = int(input("Enter a random number:"))
+# if num1 < 0:
+#     print("Your number is not greater than 0")
+# if num1 == 0:
+#     print("Your number is equal to 0")
+# if num1 > 0:
+#     print("Your number is greater than 0")
+
+
+
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("You are adult")
+if age < 18:
+    print("You've never experienced the intimacy of love")
